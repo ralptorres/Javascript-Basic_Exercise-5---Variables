d9e1b5dffd5f3e363fea4ecabdda1a7a6ca647ec
ralptorres <ralp.torres@gmail.com>
2024-11-28 20:35:08 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+let names = "Ralp";
+let cost = 5 + 2 * 3 + 9;
+
+console.log(`My name is: ${names}`);
+console.log(`Cost of food: $${cost}`);
+
+let tax = cost * 0.1;
+console.log(`Tax: $${tax}`);
+
+let totalCost = cost + tax;
+console.log(`Total cost: $${totalCost}`);
+
+let cartQuantity = 0;
+
+let calculation = "";
